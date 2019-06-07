6cd9a325bf0095d496c954fab67241cdffc231cc
jocelynerenault <jrenault@us.ibm.com>
2019-06-06 22:47:05 -0500

    added required actions for week1 assignment

--- python_code.py
+++ python_code.py
@@ -1,1 +1,6 @@
-print('Hello GitHub!')
+Capstone poject 
+This notebook will be mainly used for the capstone project.
+It will push the Notebook to my Github repository and submit a link to the notebook on your Github repository for validation by a peer
+import pandas as pd
+import numpy as np
+print('Hello Capstone Project Course!')
